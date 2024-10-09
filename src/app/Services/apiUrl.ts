079fa72b7acf0001526120135c96bb34529da9af
NdeyeMbombeCisse <ndeyecisse188@gmail.com>
2024-10-09 18:02:49 +0000

    responsivite des labels pour le filtre

--- src/app/Services/apiUrl.ts
+++ src/app/Services/apiUrl.ts
@@ -1,4 +1,4 @@
-// export const apiUrl = "http://127.0.0.1:8000/api";
-// export const storageUrl = "http://127.0.0.1:8000/storage";
-export const apiUrl = "https://backendcertification.ndeyecisse188.simplonfabriques.com/api";
-export const storageUrl = "https://backendcertification.ndeyecisse188.simplonfabriques.com/storage";
+export const apiUrl = "http://127.0.0.1:8000/api";
+export const storageUrl = "http://127.0.0.1:8000/storage";
+// export const apiUrl = "https://backendcertification.ndeyecisse188.simplonfabriques.com/api";
+// export const storageUrl = "https://backendcertification.ndeyecisse188.simplonfabriques.com/storage";
