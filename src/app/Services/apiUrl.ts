f62ed8f861f63a5ed9a72c32054df7ad07addc24
NdeyeMbombeCisse <ndeyecisse188@gmail.com>
2024-10-09 17:32:14 +0000

    gestion url

--- src/app/Services/apiUrl.ts
+++ src/app/Services/apiUrl.ts
@@ -1,2 +1,4 @@
-export const apiUrl = "http://127.0.0.1:8000/api";
-export const storageUrl = "http://127.0.0.1:8000/storage";
+// export const apiUrl = "http://127.0.0.1:8000/api";
+// export const storageUrl = "http://127.0.0.1:8000/storage";
+export const apiUrl = "https://backendcertification.ndeyecisse188.simplonfabriques.com/api";
+export const storageUrl = "https://backendcertification.ndeyecisse188.simplonfabriques.com/storage";
